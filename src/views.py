34c02a1ea7b50bd14325075de6efd9819111dcff
Аляутдинов Руслан <rusalyautdinov@yandex.ru>
2024-07-26 19:30:01 +0300

    add func get_cards_info in views.py

--- src/views.py
+++ src/views.py
@@ -18,3 +18,25 @@ def get_data_filter_by_date(file_name: str, end_date: datetime.datetime) -> pd.D
     df_date = pd.to_datetime(operations["Дата операции"], format="%d.%m.%Y %H:%M:%S")
     operations = operations[(start_date <= df_date) & (df_date <= end_date)]
     return operations
+
+
+def get_cards_info(operations: pd.DataFrame) -> list[dict]:
+    """
+    Получение информации по картам (последние 4 цифры, общая сумма расходов, кешбэк)
+    """
+    expenses = operations[operations["Сумма операции"] < 0]
+    expenses_group_cards = expenses.groupby("Номер карты").agg({"Сумма операции": "sum", "Кэшбэк": "sum"})
+
+    fieldnames = ["last_digits", "total_spent", "cashback"]
+    card_numbers = expenses_group_cards.index.tolist()
+    cards_list = []
+    for row in expenses_group_cards.iterrows():
+        card_dict = {"last_digits": card_num[-4:] for card_num in card_numbers}
+        for i, element in enumerate(row[1], 1):
+            card_dict[fieldnames[i]] = element
+        cards_list.append(card_dict)
+
+    for card in cards_list:
+        card["total_spent"] *= -1
+        card["total_spent"] = round(card["total_spent"], 2)
+    return cards_list
